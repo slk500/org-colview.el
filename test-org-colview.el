6fc3efb23d5f3392ae8cb798b033419150000a73
Sławomir Grochowski <slawomir.grochowski@gmail.com>
2023-05-14 23:46:31 +0200

    [fix] test-org-colview.el parenthesis mismatch

--- test-org-colview.el
+++ test-org-colview.el
@@ -35,13 +35,13 @@
 	     ;; Number 18 is value of point where string "* heading" starts in buffer
 	     ;; This number does not matter for `org-columns--set-widths' function or this test
              '((18 (("ITEM" "ITEM" nil nil nil) "heading" "* heading")))
-             '(("ITEM" "ITEM" nil nil nil))))
+             '(("ITEM" "ITEM" nil nil nil)))))
      ;; When width is specified
      (should
       (equal [25]
 	     (org-columns--set-widths
              '((18 (("ITEM" "ITEM" 25 nil nil) "heading" "* heading")))
-             '(("ITEM" "ITEM" 25 nil nil)))))))
+             '(("ITEM" "ITEM" 25 nil nil))))))
 
 (ert-deftest test-org-colview/uncompile-format ()
   "Test `org-columns-uncompile-format' specifications."
